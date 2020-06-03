b2d6035cd35c5fc969a958b0907f30cec27527ee
Logan Sorensen <loganmsorensen@gmail.com>
2020-06-03 14:24:56 -0700

    completed expensive_seq and word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,17 @@
 def word_count(s):
-    # Your code here
-
+    string = s.replace('"', '').replace(':', '').replace(';', '').replace(',', '').replace('.', '').replace('-', '').replace('+', '').replace('=', '').replace('/', '').replace(
+        "|", '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace('(', '').replace(')', '').replace('*', '').replace('^', '').replace('&', '').replace('\\', '')
+    if string == '':
+        # print(f'string:{string}.')
+        return {}
+    split = string.lower().split()
+    words = {i: split.count(i) for i in split}
+    return words
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
