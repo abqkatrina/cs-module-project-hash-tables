28b32887af50249754d0af4bf24b1525f5067840
Logan Sorensen <loganmsorensen@gmail.com>
2020-06-03 16:15:17 -0700

    no_dups completed

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,15 @@
-def no_dups(s):
-    # Your code here
 
+
+def no_dups(s):
+    list_of_words = []
+
+    split = s.split()
+
+    for i in split:
+        if i not in list_of_words:
+            list_of_words.append(i)
+
+    return ' '.join(list_of_words)
 
 
 if __name__ == "__main__":
